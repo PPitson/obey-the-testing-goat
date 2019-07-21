4a292ad2682de48ecf8c1468a6a376931918ae38
PPitson <pitu5599@gmail.com>
2019-07-21 08:48:27 +0200

    Move comment inside a test method

--- functional_tests.py
+++ functional_tests.py
@@ -13,10 +13,9 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    # Edith has heard about a cool new online to-do app. She goes
-    # to check out its homepage
-
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
